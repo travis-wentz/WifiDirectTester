bd197b628c21d7d672df9b388517d3c4f333810b
travis-wentz <wentz.travis@gmail.com>
2017-04-11 13:12:41 -0600

    Sets up peer-to-peer manager.

--- app/src/main/java/com/example/traviswentz/wifidirecttester/MainActivity.java
+++ app/src/main/java/com/example/traviswentz/wifidirecttester/MainActivity.java
@@ -2,12 +2,36 @@ package com.example.traviswentz.wifidirecttester;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.IntentFilter;
+import android.net.wifi.p2p.WifiP2pManager;
+import android.content.Context;
+import android.net.wifi.p2p.WifiP2pManager.Channel;
 
 public class MainActivity extends AppCompatActivity {
 
+    private final IntentFilter intentFilter = new IntentFilter();
+    private WifiP2pManager manager;
+    private Channel channel;
+
+    // might have to unicast to each peer on the list if broadcast is not an option?
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //  Indicates a change in the Wi-Fi P2P status.
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
+
+        // Indicates a change in the list of available peers.
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
+
+        // Indicates the state of Wi-Fi P2P connectivity has changed.
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
+
+        // Indicates this device's details have changed.
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
+
+        manager = (WifiP2pManager) getSystemService(Context.WIFI_P2P_SERVICE);
+        channel = manager.initialize(this, getMainLooper(), null);
     }
 }
